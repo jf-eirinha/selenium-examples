df40383f9e0240945bb516a39a37a2217e2f7d5f
Joao Eirinha <joaoeirinh@gmail.com>
2019-04-16 18:31:55 +0100

    added flag to example 2. madeg gif of example 1

--- example2/auto-clicker-stitcher.py
+++ example2/auto-clicker-stitcher.py
@@ -6,6 +6,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
+from absl import flags
+from absl import app
 
 options = Options()
 options.add_argument("start-maximized")
@@ -13,61 +15,70 @@ options.add_argument("disable-infobars")
 options.add_argument("--disable-extensions")
 options.add_argument("--lang=en-us")
 
-# Using Chrome to access the web
-driver = webdriver.Chrome(options=options, 
-	executable_path='C:/Users/joao_/Downloads/chromedriver_win32/chromedriver.exe')
+# Flags
+FLAGS = flags.FLAGS
+flags.DEFINE_string('exe_path', 'C:/Users/Morty/Downloads/chromedriver_win32/chromedriver.exe', 'Path to Chrome Driver.')
 
-# Open the website
-driver.get('https://www.stitcher.com')
+def main(argv):
 
-# Select button by class
-search_button = driver.find_element_by_class_name('searchButton')
+	# Initialize Web Driver
+	driver = webdriver.Chrome(options=options, 
+		executable_path=FLAGS.exe_path)
 
-# Click the search button
-search_button.click()
+	# Open the website
+	driver.get('https://www.stitcher.com')
 
-# Select input box
-input_box = driver.find_element_by_id('search')
+	# Select button by class
+	search_button = driver.find_element_by_class_name('searchButton')
 
-# Input the name of the podcast
-podcast = input("What is the name of the podcast? ")
+	# Click the search button
+	search_button.click()
 
-# Send text to search
-input_box.send_keys(podcast)
+	# Select input box
+	input_box = driver.find_element_by_id('search')
 
-# Click the search button again
-search_button.click()
+	# Input the name of the podcast
+	podcast = input("What is the name of the podcast? ")
 
-# Define page loading time delay
-delay = 5
+	# Send text to search
+	input_box.send_keys(podcast)
 
-# Search for list of results
-try:
-    search_results = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.ID, 'showResultsList')))
-    print ("Page is ready!")
-except TimeoutException:
-    print ("Loading took too much time!")
+	# Click the search button again
+	search_button.click()
 
-# Search podcast image
-try:
-    img = WebDriverWait(search_results, delay).until(EC.presence_of_element_located((By.TAG_NAME, 'img')))
-    print ("Page is ready!")
-except TimeoutException:
-    print ("Loading took too much time!")
+	# Define page loading time delay
+	delay = 5
 
-# Click image
-img.click()
-
-# Load more episodes loop
-found = True
-
-# Try/except to load click button each page reload
-while found:
-
+	# Search for list of results
 	try:
-		load_more_button = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.LINK_TEXT, 'Load More Episodes')))
-		load_more_button.click()
-		print("Click")
+		search_results = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.ID, 'showResultsList')))
+		print ("Page is ready!")
 	except TimeoutException:
-		print ("You've reached the end OR page was still loading")
-		found = False
+		print ("Loading took too much time!")
+
+	# Search podcast image
+	try:
+		img = WebDriverWait(search_results, delay).until(EC.presence_of_element_located((By.TAG_NAME, 'img')))
+		print ("Page is ready!")
+	except TimeoutException:
+		print ("Loading took too much time!")
+
+	# Click image
+	img.click()
+
+	# Load more episodes loop
+	found = True
+
+	# Try/except to load click button each page reload
+	while found:
+
+		try:
+			load_more_button = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.LINK_TEXT, 'Load More Episodes')))
+			load_more_button.click()
+			print("Click")
+		except TimeoutException:
+			print ("You've reached the end OR page was still loading")
+			found = False
+
+if __name__ == '__main__':
+        app.run(main)
